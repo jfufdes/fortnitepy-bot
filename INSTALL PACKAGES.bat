e21715ba70b8351a650826d8b95a7efc696b90b6
KaosDrip <55036780+KaosDrip@users.noreply.github.com>
2020-05-15 19:46:13 -0500

    Update INSTALL PACKAGES.bat

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -2,7 +2,7 @@
 ECHO Installing the required packages for the bot!
 TIMEOUT 3
 
-py -3 -m pip install -U fortnitepy
+py -3 -m pip install -U fortnitepy==1.7.1
 py -3 -m pip install -U aiohttp
 py -3 -m pip install -U colorama
 py -3 -m pip install -U BenBotAsync
